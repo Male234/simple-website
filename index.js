a43e96bee27d0868a4761133b5c4f417d174b2b5
Malexz <90273612+Male234@users.noreply.github.com>
2022-12-05 16:53:05 +0100

    Main file
    main files

---
+++ index.js
@@ -0,0 +1,35 @@
+let userName = window.prompt("Wprowadz imie: ");
+
+setTimeout(() => {
+    
+}, timeout);
+
+while(userName == "" || userName == null){
+    userName = window.prompt("Enter your name: ")
+    
+}
+
+// ! oh no!
+// TODO help myself
+// ? why
+// * ?
+// TODO help
+
+// * Show name
+document.getElementById("showNameBtn").onclick = function(){
+    document.getElementById("bruh").innerHTML = userName
+    console.warn("Name shown!")
+}
+// * Hide name
+document.getElementById("hideNameBtn").onclick = function(){
+    document.getElementById("bruh").innerHTML = ""
+    console.log("Name hidden")
+    console.error("lol")
+}
+/*
+console.log(userName.length);
+console.log(userName);
+console.log(userName.charAt(0));
+console.log(userName.indexOf('M'));
+console.log(userName.lastIndexOf('e'));
+*/
